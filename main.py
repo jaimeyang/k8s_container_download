c1f780a4baa591bf804f63a8e813afc48324ec39
jaimeyang <jaimeyang1990@gmail.com>
2019-01-09 19:13:41 +0800

    build script

--- main.py
+++ main.py
@@ -2,18 +2,94 @@
 #-*- coding:utf-8 -*-
 
 import yaml
+import os
+import sys
+import socket
+import zipfile
+# path of download config yaml
+configPath = "./kubespray-2.8.1/roles/download/defaults/main.yml"
+# binary file name
+kubeadm_download_url = ""
+hyperkube_download_url = ""
+etcd_download_url = ""
+cni_download_url = ""
+# registry config
+# /var/lib/registry
+var_lib = ""
+docker_reg = ""
+# temp registry addr
+registry = ""
+# ansible 配置
+ansibleConf = "inventory/netcluster/hosts.ini"
 
-configPath = "F:\\k8s\kubespray-2.8.1\\kubespray-2.8.1\\roles\download\defaults\\main.yml"
 
+def getLocalIp():
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    return ip
+
+
+def downLoadServer():
+    command = "apt-get install apache2"
+    os.system(command)
+    os.mkdir("/var/www/html/package")
+    ln = "ln -s ./package /var/www/html/package"
+    os.system(ln)
+    addr = "http://" + getLocalIp() + "/package"
+    return addr
+
+def registryDocker():
+    comamnd = "docker run -d \
+    -p 5000:5000 \
+    -p 443:443 \
+    -v " + var_lib + ":/var/lib/registry \
+    -v " + docker_reg + ":/etc/docker/registry \
+    registry.docker-cn.com/library/registry:2"
+    os.system(comamnd)
 
 
 def parseYaml():
     version_map = []
     with open(configPath,"r") as f:
         data = yaml.load(f)
-    # for key,value in data.items():
+        version_map = data.items()
+    version_map["registry"] = registry
+    version_map["kubeadm_download_url"] = kubeadm_download_url
+    version_map["hyperkube_download_url"] = hyperkube_download_url
+    version_map["etcd_download_url"] = etcd_download_url
+    version_map["cni_download_url"] = cni_download_url
 
+def pushImage():
+    zipPath = "./k8s_offline.zip"
+    zfile = zipfile.ZipFile(zipPath)
+    temp = "./temp"
+    os.mkdir(temp)
+    for names in zfile.namelist():
+        zfile.extract(names,temp)
+    zfile.close()
+    files = os.listdir(temp)
+    for file in files:
+        imageName = file.replace("#","/")
+        imageName = registry + "/" + imageName
+        comand = "docker load " + file + " > " + " " + imageName
+        p = os.popen(comand)
+        print( p.read() )
+        pushCommand = "docker push " + imageName
+        p1 = os.popen(pushCommand)
+        print( p1.read() )
+
+def runAnsible():
+    command = "ansible-playbook -i " + ansibleConf + " " + " cluster.yml -b -v --private-key=~/.ssh/id_rsa"
+    p = os.popen(command)
+    print(p.read())
 
 if __name__ == "__main__":
+    addr = downLoadServer()
+    kubeadm_download_url = addr + kubeadm_download_url
+    hyperkube_download_url = addr + hyperkube_download_url
+    etcd_download_url = addr + etcd_download_url
+    cni_download_url = addr + cni_download_url
     parseYaml()
+    pushImage()
+    runAnsible()
     pass
